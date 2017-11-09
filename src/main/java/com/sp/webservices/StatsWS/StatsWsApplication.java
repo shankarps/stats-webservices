70ad06a07c499f0b838e2864e6a928c6bd8873a4
Shankar <shankar.pachari@gmail.com>
2017-11-08 19:49:11 -0800

    first draft mnpn

---
+++ src/main/java/com/sp/webservices/StatsWS/StatsWsApplication.java
@@ -0,0 +1,12 @@
+package com.sp.webservices.StatsWS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatsWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatsWsApplication.class, args);
+	}
+}
